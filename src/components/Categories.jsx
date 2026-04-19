4e58dda6d566109164279e48cbf3fad3dc1555ee
masflame <tumishopraise03@gmail.com>
2026-04-19 13:06:29 +0200

    feat: add Cartier rings collection (84 items, USD converted to ZAR)
    - Added rings-cartier JSON/CSV + images
    - Converted USD prices to ZAR (rate 18.5, ,300 -> R560,550)
    - Rebuilt products.js: 1475 products total (184 rings, up from 103)
    - Updated category card images to use background photos

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 
 import watchesImg from '../assets/shop/watches/rolex/rolex-datejust-36-unisex-watch-3/rolex-datejust-36-unisex-watch/biJr2JOdOcRapA8_a4cccce7-6201-4c34-b4d8-fc76d8d25197.jpg';
-import ringsImg from '../assets/shop/rings/engagement-rings-1/0-23ct-lab-grown-wedding-band-set-in-9ct-yellow-gold-copy-1/2-27ct-lab-grown-diamond-asscher-cut-set-in-platinum/1_393c7a80-5043-4ae9-ae64-5e82b27ea7c8.jpg';
-import necklacesImg from '../assets/shop/necklaces/necklaces-A/bangle-italian-made-70538/flower-necklace-set-in-18ct-white-gold/1_d930c289-e058-4a12-b1fd-eeb2682dea0d.jpg';
-import earringsImg from '../assets/shop/earings/earrings/0-50ct-lab-grown-round-brilliant-diamond-earrings-set-in-9ct-yellow-gold-1ct-total-diamond-weight/1-5ct-lab-grown-round-brilliant-diamond-earrings-set-in-9ct-white-gold-3ct-total/1_2733a5a5-ce16-4c03-9b42-a5dcd790d979.jpg';
-import pendantsImg from '../assets/shop/pendants/pendants/3-stone-emerald-cut-pendant-32609/emerald-cut-pendant/32609_4e38456a-6d8f-4b38-9ff2-bc728a9afa91.jpg';
-import weddingBandsImg from '../assets/shop/wedding bands/wedding-bands-test/straight-diamond-wedding-band-51096-w-10/diamond-wedding-band/51096-W.jpg';
-import braceletsImg from '../assets/shop/bracelets/bracelets/flower-necklace-set-in-18ct-yellow-gold-copy-2/flower-bracelet-set-in-18ct-white-gold-turquoise/1_4a334074-f3eb-4060-8b18-8ae7653a78c9.jpg';
+import ringsImg from '../assets/background/rings.jpg';
+import necklacesImg from '../assets/background/necklaces.jpg';
+import earringsImg from '../assets/background/earings.jpg';
+import pendantsImg from '../assets/background/pendants.jpg';
+import weddingBandsImg from '../assets/background/wedding bands.jpg';
+import braceletsImg from '../assets/background/bracelets.jpg';
 
 const items = [
   { id: 'watches', name: 'Watches', image: watchesImg },
