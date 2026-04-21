f607e66636528884e56ec77e094a6c61167e4da4
masflame <tumishopraise03@gmail.com>
2026-04-21 21:18:48 +0200

    Hotfix PayFast live credential fallbacks for launch

--- src/utils/payfast.js
+++ src/utils/payfast.js
@@ -1,8 +1,8 @@
 import md5 from 'md5';
 
-const MERCHANT_ID = import.meta.env.VITE_PAYFAST_MERCHANT_ID || '';
-const MERCHANT_KEY = import.meta.env.VITE_PAYFAST_MERCHANT_KEY || '';
-const PASSPHRASE = import.meta.env.VITE_PAYFAST_PASSPHRASE || '';
+const MERCHANT_ID = import.meta.env.VITE_PAYFAST_MERCHANT_ID || '17842910';
+const MERCHANT_KEY = import.meta.env.VITE_PAYFAST_MERCHANT_KEY || 'pxx7pmq1m72tp';
+const PASSPHRASE = import.meta.env.VITE_PAYFAST_PASSPHRASE || 'Letebele2003';
 const PAYFAST_URL = import.meta.env.VITE_PAYFAST_URL || 'https://www.payfast.co.za/eng/process';
 
 function assertPayfastConfig() {
